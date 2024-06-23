4c56236b5ceddb5dfa124b2dc59d8204ee03d2ba
Liv <kibibyt3@proton.me>
2024-06-22 17:16:28 -0700

    get rid of quotation escapes

--- src/main.c
+++ src/main.c
@@ -4,37 +4,41 @@ int main() {
 	char *text = "#include <stdio.h>\n\
 #include <string.h>\n\
 int main() {\n\
-	char *text = \"?\";\n\
+	char *text = ?;\n\
+	const char format_one_chars[] = {'%', 'c', 0};\n\
+	const char format_four_chars[] = {'%', 'c', '%', 'c', '%', 'c', '%', 'c', 0};\n\
 	for (int i = 0; i < strlen(text); i++) {\n\
 		if (text[i] == (char) 63) {\n\
+			printf(format_one_chars, 34);\n\
 			for (int j = 0; j < strlen(text); j++) {\n\
-				if (text[j] == '\"') {\n\
-					printf(\"%c%c\", (char) 92, (char) 34);\n\
-				} else if (text[j] == 0x0A) {\n\
-					printf(\"%c%c%c%c\", 92, 110, 92, 0x0A); \n\
+				if (text[j] == 0x0A) {\n\
+					printf(format_four_chars, 92, 110, 92, 0x0A); \n\
 				} else {\n\
-					printf(\"%c\", text[j]);\n\
+					printf(format_one_chars, text[j]);\n\
 				}\n\
 			}\n\
+			printf(format_one_chars, 34);\n\
 		} else {\n\
-			printf(\"%c\", text[i]);\n\
+			printf(format_one_chars, text[i]);\n\
 		}\n\
 	}\n\
 }\n\
 ";
+	const char format_one_chars[] = {'%', 'c', 0};
+	const char format_four_chars[] = {'%', 'c', '%', 'c', '%', 'c', '%', 'c', 0};
 	for (int i = 0; i < strlen(text); i++) {
 		if (text[i] == (char) 63) {
+			printf(format_one_chars, 34);
 			for (int j = 0; j < strlen(text); j++) {
-				if (text[j] == '"') {
-					printf("%c%c", (char) 92, (char) 34);
-				} else if (text[j] == 0x0A) {
-					printf("%c%c%c%c", 92, 110, 92, 0x0A); 
+				if (text[j] == 0x0A) {
+					printf(format_four_chars, 92, 110, 92, 0x0A); 
 				} else {
-					printf("%c", text[j]);
+					printf(format_one_chars, text[j]);
 				}
 			}
+			printf(format_one_chars, 34);
 		} else {
-			printf("%c", text[i]);
+			printf(format_one_chars, text[i]);
 		}
 	}
 }
